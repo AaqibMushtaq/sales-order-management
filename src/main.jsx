0762137383806205358b2f3e61d2d53848a7f548
SheikhSuhail <dev.sheikhsuhail@gmail.com>
2024-06-10 11:00:14 +0530

    Add ChakraUI, Add Example Element

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
       <ReactQueryDevtools initialIsOpen />
     </QueryClientProvider>
   </React.StrictMode>
